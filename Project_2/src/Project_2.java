05c9158225705afea4df4aba714df3641099d32c
irusli2 <31106571+irusli2@users.noreply.github.com>
2017-08-23 10:59:03 -0700

    hi there

---
+++ Project_2/src/Project_2.java
@@ -0,0 +1,13 @@
+//Isabelle Rusli 
+//August 23, 2017
+//Project 2 -- use print and println
+public class Project_2 {
+
+	public static void main(String[] args) {
+		System.out.println ("hi there");
+		System.out.print ("hi there");
+		//TODO Auto-generated method stub
+
+	}
+
+}
